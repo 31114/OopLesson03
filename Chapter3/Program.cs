d32a88505e01b79645a62d571f2e83e4e753e924
AzumaUno <31114@ojs.ac.jp>
2020-09-24 11:26:00 +0900

    2020/09/24

---
+++ Chapter3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    class Program
+    {
+        public delegate bool Judgement(int value);
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 5, 3, 9, 1, 7, 2, 3, 8, 9, 7, 4, 1, 2, 3, 8, 4, 5 };
+
+            //Judgement judge = IsEven;
+            //int count = Count(numbers, delegate(int n)
+            //    {
+            //        return (n % 2 == 0);
+            //    });
+            int count = Count(numbers, (int n) => { return (n >= 5); });
+            Console.WriteLine($"偶数の数は{count}です");
+        }
+
+        //nが偶数かどうかを調べる
+        public static bool IsEven(int n )
+        {
+            //同じらしいよ
+            return (n % 2 == 0);
+            //if(n % 2 == 0)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+        public static int Count(int[] numbers, Judgement judge)
+        {
+            int count = 0;
+            foreach (var n in numbers)
+            {
+                if (judge(n) == true)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
