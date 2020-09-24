c144f8437b14df15c788006444e4062d2d10f67c
AzumaUno <31114@ojs.ac.jp>
2020-09-24 11:58:15 +0900

    Fight Fire with Fire

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,45 +11,15 @@ namespace Chapter3
 {
     class Program
     {
-        public delegate bool Judgement(int value);
         static void Main(string[] args)
         {
-            var numbers = new[] { 5, 3, 9, 1, 7, 2, 3, 8, 9, 7, 4, 1, 2, 3, 8, 4, 5 };
-
-            //Judgement judge = IsEven;
-            //int count = Count(numbers, delegate(int n)
-            //    {
-            //        return (n % 2 == 0);
-            //    });
-            int count = Count(numbers, (int n) => { return (n >= 5); });
-            Console.WriteLine($"偶数の数は{count}です");
-        }
-
-        //nが偶数かどうかを調べる
-        public static bool IsEven(int n )
-        {
-            //同じらしいよ
-            return (n % 2 == 0);
-            //if(n % 2 == 0)
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-        }
-        public static int Count(int[] numbers, Judgement judge)
-        {
-            int count = 0;
-            foreach (var n in numbers)
+            var list = new List<string>
             {
-                if (judge(n) == true)
-                {
-                    count++;
-                }
-            }
-            return count;
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Moscow","Warsaw","Hong Kong",
+            };
+
+            list.ConvertAll(s => s.ToUpper()).ForEach(s => Console.WriteLine(s));
+            
         }
     }
 }
