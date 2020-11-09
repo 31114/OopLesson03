303714ce774b0487d80327518dd651aed161298f
31114 <31114@ojs.ac.jp>
2020-11-09 14:07:36 +0900

    1106

---
+++ SendMailApp/ConfigWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SendMailApp
+{
+    /// <summary>
+    /// ConfigWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        public ConfigWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btDefault_Click(object sender, RoutedEventArgs e)
+        {
+            Config cf = (Config.GetInstance()).getDefaultStatus();
+
+            tbSmtp.Text = cf.Smtp;
+            tbPort.Text = cf.Port.ToString();
+            tbSender.Text = tbUserName.Text = cf.MailAddress;
+            tbPassWord.Password = cf.PassWord;
+            cbSsl.IsChecked = cf.Ssl;
+        }
+        //適用（更新）
+        private void btApply_Click(object sender, RoutedEventArgs e)
+        {
+            (Config.GetInstance()).UpdateStatus(
+                tbSmtp.Text,
+                tbUserName.Text,
+                tbPassWord.Password,
+                int.Parse(tbPort.Text),
+                cbSsl.IsChecked ?? false);   //更新処理を呼び出す
+        }
+        //OKボタン
+        private void btOk_Click(object sender, RoutedEventArgs e)
+        {
+            btApply_Click(sender, e);   //更新処理を呼び出す
+            this.Close();
+        }
+        //キャンセルボタン
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //ロード時に一度だけ呼び出される
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,60 @@ namespace SendMailApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SmtpClient sc = new SmtpClient();
+
         public MainWindow()
         {
             InitializeComponent();
+            sc.SendCompleted += Sc_SendCompleted;
         }
+
+        private void Sc_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Send message Canceled");
+            }
+            else
+            {
+                MessageBox.Show("Send message Completed");
+            }
+        }
+
+        private void btOk_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage msg = new MailMessage("ojsinfosys01@gmail.com" , tbTo.Text);
+
+                msg.Subject = tbTitle.Text; //件名
+                msg.Body = tbBody.Text; //本文
+
+                sc.Host = "smtp.gmail.com"; //SMTPサーバの設定
+                sc.Port = 587;
+                sc.EnableSsl = true;
+                sc.Credentials = new NetworkCredential("ojsinfosys01@gmail.com" , "ojsInfosys2020");
+
+                sc.SendMailAsync(msg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            sc.SendAsyncCancel();
+        }
+
+        //設定画面表示
+        private void btConfig_Click(object sender, RoutedEventArgs e)
+        {
+            ConfigWindow configWindow = new ConfigWindow();//設定画面のインスタンスを生成
+            configWindow.Show();  //表示
+        }
+
     }
 }
